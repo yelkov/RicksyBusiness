4eac64b45f152743880cc22fc9762e19a49d83c2
Sergio Prieto García <serprigar@gmail.com>
2024-04-04 12:53:04 +0200

    feat(UfosPark.java): add main methods

--- src/main/java/edu/badpals/RicksyBusiness/UfosPark.java
+++ src/main/java/edu/badpals/RicksyBusiness/UfosPark.java
@@ -1,19 +1,47 @@
 package edu.badpals.RicksyBusiness;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class UfosPark implements GuestDispatcher{
-    private List<String> flota = new ArrayList<>();
+    private final double fee = 500d;
+    private Map<String, String> flota = new HashMap<>();
     public UfosPark() {
     }
 
     public void add(String ovni) {
-        this.flota.add(ovni);
+        this.flota.put(ovni,"");
     }
 
     @Override
     public void dispatch(CreditCard creditCard) {
+        for (Map.Entry<String, String> entry: flota.entrySet()){
+            if(entry.getValue() == ""){
+                this.flota.replace(entry.getKey(), creditCard.number());
+                creditCard.setCredit(creditCard.credit() - fee);
+            }else{
+                ;
+            }
+        }
+    }
+    /*public String getUfoOf(String number){
+        return this.flota.
+    }*/
 
+    @Override
+    public String toString() {
+        return flota.keySet()
+                .stream()
+                .sorted()
+                .collect(Collectors.toList())
+                .toString();
+    }
+
+    public List<String> cardNumbers() {
+        List<String> numberList= new ArrayList<>();
+        for ( String value : this.flota.values()){
+            numberList.add(value);
+        }
+        return numberList;
     }
 }
